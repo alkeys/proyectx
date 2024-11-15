f2b2b7b5e1ef5b3f010312e033e3b2c8560a7d9a
Martha Beatriz Fajardo Mendoza <FM22010@ues.edu.sv>
2024-11-14 21:37:12 -0600

    Menu de navegación

--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/frmMenu.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/frmMenu.java
@@ -24,12 +24,11 @@ public class frmMenu implements Serializable {
                 .label("Tipos")
                 .expanded(true)
                 .build();
-
-        DefaultMenuItem itemSala = DefaultMenuItem.builder()
-                .value("Sala")
-                .icon("pi pi-desktop")
+        DefaultMenuItem itemPago = DefaultMenuItem.builder()
+                .value("Pago")
+                .icon("pi pi-credit-card")
                 .ajax(true)
-                .command("#{frmMenu.navegar('TipoSala.jsf')}")
+                .command("#{frmMenu.navegar('TipoPago.jsf')}")
                 .build();
         DefaultMenuItem itemAsiento = DefaultMenuItem.builder()
                 .value("Asiento")
@@ -37,15 +36,8 @@ public class frmMenu implements Serializable {
                 .ajax(true)
                 .command("#{frmMenu.navegar('TipoAsiento.jsf')}")
                 .build();
-        DefaultMenuItem itemReserva = DefaultMenuItem.builder()
-                .value("Reserva")
-                .icon(" pi pi-calendar-plus")
-                .ajax(true)
-                .command("#{frmMenu.navegar('TipoReserva.jsf')}")
-                .build();
-
         DefaultMenuItem itemPelicula = DefaultMenuItem.builder()
-                .value("Tipos de Película")
+                .value("Película")
                 .icon("pi pi-star")
                 .ajax(true)
                 .command("#{frmMenu.navegar('TipoPelicula.jsf')}")
@@ -56,22 +48,48 @@ public class frmMenu implements Serializable {
                 .ajax(true)
                 .command("#{frmMenu.navegar('TipoProducto.jsf')}")
                 .build();
-        DefaultMenuItem itemPago = DefaultMenuItem.builder()
-                .value("Pago")
-                .icon("pi pi-credit-card")
+        DefaultMenuItem itemReserva = DefaultMenuItem.builder()
+                .value("Reserva")
+                .icon(" pi pi-calendar-plus")
                 .ajax(true)
-                .command("#{frmMenu.navegar('TipoPago.jsf')}")
+                .command("#{frmMenu.navegar('TipoReserva.jsf')}")
                 .build();
+        DefaultMenuItem itemSala = DefaultMenuItem.builder()
+                .value("Sala")
+                .icon("pi pi-desktop")
+                .ajax(true)
+                .command("#{frmMenu.navegar('TipoSala.jsf')}")
+                .build();
+
         DefaultSubMenu Cine = DefaultSubMenu.builder()
                 .label("Cine")
                 .expanded(true)
                 .build();
         DefaultMenuItem Peliculas_Lista = DefaultMenuItem.builder()
-                .value("Peliculas")
+                .value("Pelicula")
                 .icon("pi pi-list")
                 .ajax(true)
                 .command("#{frmMenu.navegar('Pelicula.jsf')}")
                 .build();
+        DefaultMenuItem sucursal = DefaultMenuItem.builder()
+                .value("Sucursal")
+                .icon("pi pi-home")
+                .ajax(true)
+                .command("#{frmMenu.navegar('Sucursal.jsf')}")
+                .build();
+        DefaultMenuItem sala = DefaultMenuItem.builder()
+                .value("Sala")
+                .icon("pi pi-desktop")
+                .ajax(true)
+                .command("#{frmMenu.navegar('Sala.jsf')}")
+                .build();
+        DefaultMenuItem reserva = DefaultMenuItem.builder()
+                .value("Reserva")
+                .icon("pi pi-calendar-plus")
+                .ajax(true)
+                .command("#{frmMenu.navegar('Reserva.jsf')}")
+                .build();
+
 
         tipos.getElements().add(itemSala);
         tipos.getElements().add(itemAsiento);
@@ -80,8 +98,12 @@ public class frmMenu implements Serializable {
         tipos.getElements().add(itemProducto);
         tipos.getElements().add(itemPago);
         Cine.getElements().add(Peliculas_Lista);
-        model.getElements().add(Cine);
+        Cine.getElements().add(sucursal);
+        Cine.getElements().add(sala);
+        Cine.getElements().add(reserva);
         model.getElements().add(tipos);
+        model.getElements().add(Cine);
+
 
 
 
